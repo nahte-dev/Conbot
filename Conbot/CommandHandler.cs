b6396211073f59ae71e3f26efaa9f13cc16e2013
Ethan <ethanmorris7335dev@gmail.com>
2021-08-06 20:11:08 +1200

    Initial Commit

---
+++ Conbot/CommandHandler.cs
@@ -0,0 +1,60 @@
+﻿using Discord.WebSocket;
+using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using System.Reflection;
+
+namespace Conbot
+{
+    class CommandHandler
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+
+        // Retrieve client and CommandService instance
+        public CommandHandler(DiscordSocketClient client, CommandService commands)
+        {
+            _commands = commands;
+            _client = client;
+        }
+
+        public async Task InstallCommandAsync()
+        {
+            // Hook MessageReceived event into command handler
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), services: null);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            // Skip if command was system message
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+
+            // Create number to identify prefix and command index
+            int argPos = 0;
+
+            // Determine if message is command based on prefix
+            if (!(message.HasCharPrefix('!', ref argPos)
+                || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+                || message.Author.IsBot)
+
+                return;
+
+            // Create Websocket based command context based on the message
+            var context = new SocketCommandContext(_client, message);
+
+            // Execute command with the context just created along with service provider 
+            // pre-condition checks
+            await _commands.ExecuteAsync(
+                context: context,
+                argPos: argPos,
+                services: null);
+        }
+    }
+}
---
+++ Conbot/Program.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace Conbot
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+        
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            var TOKEN = "ODcyMzEyMDk0NzQwMDc0NTA3.YQoB8g.KMN_Yh8KZrxIQA6L5GcWr5GYKEE";
+
+            await _client.LoginAsync(TokenType.Bot, TOKEN);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
