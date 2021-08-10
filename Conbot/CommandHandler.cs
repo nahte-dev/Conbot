af77f6efc5d612b09fda6b31695be5bbe9011ffe
Ethan <ethanmorris7335dev@gmail.com>
2021-08-10 15:20:46 +1200

    Implementing command functionality
    Removed bot token for public commit

--- Conbot/CommandHandler.cs
+++ Conbot/CommandHandler.cs
@@ -57,4 +57,13 @@ namespace Conbot
                 services: null);
         }
     }
+
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        // say hello world
+        [Command("say")]
+        [Summary("Echoes a message")]
+        public Task SayAsync([Remainder][Summary("The text to echo")] string echo)
+            => ReplyAsync(echo);
+    }
 }
--- Conbot/Program.cs
+++ Conbot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
 using System;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace Conbot
     class Program
     {
         private DiscordSocketClient _client;
-
+        private CommandService _commands;
+        private CommandHandler cmd;
+       
         static void Main(string[] args)
             => new Program().MainAsync().GetAwaiter().GetResult();
         
@@ -16,9 +19,10 @@ namespace Conbot
         public async Task MainAsync()
         {
             _client = new DiscordSocketClient();
+            _commands = new CommandService();
             _client.Log += Log;
 
-            var TOKEN = "ODcyMzEyMDk0NzQwMDc0NTA3.YQoB8g.KMN_Yh8KZrxIQA6L5GcWr5GYKEE";
+            var TOKEN = "";
 
             await _client.LoginAsync(TokenType.Bot, TOKEN);
             await _client.StartAsync();
@@ -30,6 +34,26 @@ namespace Conbot
         {
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage message)
+        {
+            var msg = message as SocketUserMessage;
+            var context = new SocketCommandContext(_client, msg);
+
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if (msg.HasStringPrefix("!", ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+            }
         }
     }
 }
